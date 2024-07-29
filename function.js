64f65f2ead78e0d89efd29f1d87d5ebedd9cc762
Nagireddysailaja <158279136+Nagireddysailaja@users.noreply.github.com>
2024-07-29 20:08:52 +0530

    Update function.js

--- function.js
+++ function.js
@@ -1,7 +1,5 @@
 
-//Study synchronous and asynchronous programming: Callbacks, Promises, and async/await. Implement examples for each.
-//Convert callback-based code to use Promises and async/await.
-//synchronous 
+ //synchronous and Asynchronous
 function lucky(name) {
     console.log(`Hello, ${name}!`);
 }
@@ -14,12 +12,8 @@ function vicky(name) {
 
 vicky("beauty");
 
-//if we pass number of task but it will be excute step by step that is known as synchronous
-//if we pass number of task but it will be excute at a time that is known as asynchronous
-// it will be use the functions  callback,promise,await:async
 
 // Asynchronous with callback
-// a function with call another function that also known as callback function.
 function fetchData(callback) {
     setTimeout(() => {
         const data = { name: "vicky", age: 25 };
@@ -32,9 +26,6 @@ fetchData((data) => {
 });
 
 
-// Creating a Promise
-// promise is a object it will be return the function and excute that is also known as promise and if there are 
-// three status pending ,successful, rejected.
 function fetchData() {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
